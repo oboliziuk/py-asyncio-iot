6ee44865364ba5747513e99465d0dd927a6cca8f
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2025-05-19 12:21:01 +0300

    Refactor IOTService fully async and fix Device protocol annotations

--- app/iot/service.py
+++ app/iot/service.py
@@ -1,7 +1,7 @@
 import random
 import string
 import asyncio
-from typing import Protocol
+from typing import Protocol, Awaitable
 
 from .message import Message, MessageType
 
@@ -13,13 +13,13 @@ def generate_id(length: int = 8) -> str:
 # Protocol is very similar to ABC, but uses duck typing
 # so devices should not inherit for it (if it walks like a duck, and quacks like a duck, it's a duck)
 class Device(Protocol):
-    def connect(self) -> None:
+    async def connect(self) -> None:
         ...  # Ellipsis - similar to "pass", but sometimes has different meaning
 
-    def disconnect(self) -> None:
+    async def disconnect(self) -> None:
         ...
 
-    def send_message(self, message_type: MessageType, data: str) -> None:
+    async def send_message(self, message_type: MessageType, data: str) -> None:
         ...
 
 
@@ -33,9 +33,10 @@ class IOTService:
         self.devices[device_id] = device
         return device_id
 
-    def unregister_device(self, device_id: str) -> None:
-        self.devices[device_id].disconnect()
-        del self.devices[device_id]
+    async def unregister_device(self, device_id: str) -> None:
+        device = self.devices.pop(device_id, None)
+        if device:
+            await device.disconnect()
 
     def get_device(self, device_id: str) -> Device:
         return self.devices[device_id]
